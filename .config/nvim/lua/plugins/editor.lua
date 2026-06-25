84da1d146a2839d7e569cfc1695c9726dc5c3339
Phillipe Sebastiao <phillipe.sebastiao@gmail.com>
2026-06-26 01:34:02 +1000

    add notificatoins, move plugins

---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,36 @@
+return {
+    { "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" }, opts = {} },
+    { "nvim-mini/mini.icons", version = false, opts = {} },
+    { "nvim-mini/mini.statusline", version = false, opts = {} },
+    { "folke/which-key.nvim", event = "VeryLazy", opts = {} },
+    { "nvim-mini/mini.indentscope", event = { "BufReadPre", "BufNewFile" }, version = false, opts = { symbol = "┃", } },
+    {
+        "nvim-mini/mini.notify",
+        version = "*",
+        config = function()
+            require("mini.notify").setup({
+                -- only show the message
+                content = {
+                    format = function(notif)
+                        return notif.msg
+                    end,
+                },
+                -- make transparent and move to bottom right corner
+                window = {
+                    winblend = 0,
+                    config = function()
+                        return {
+                            title = "",
+                            anchor = "SE",
+                            row = vim.o.lines - 3,
+                            col = vim.o.columns - 2,
+                            border = "none",
+                        }
+                    end,
+                },
+            })
+            -- remove background
+            vim.api.nvim_set_hl(0, "MiniNotifyNormal", { bg = "NONE" })
+        end,
+    },
+}
---
+++ .config/nvim/lua/plugins/explorer.lua
@@ -0,0 +1,12 @@
+return {
+    "nvim-mini/mini.files",
+    version = false,
+    keys = {
+        {
+            "<leader>e",
+            "<cmd>lua MiniFiles.open()<CR>",
+            desc = "Open file explorer",
+        },
+    },
+    opts = {},
+}
