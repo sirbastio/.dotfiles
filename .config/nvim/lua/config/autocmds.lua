ef47ba8712ee0b8e2f3b4d99101d12141000178c
Phillipe Sebastiao <phillipe.sebastiao@gmail.com>
2026-07-08 15:50:27 +1000

    fix: codediff starting with open folds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,23 +1,6 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-local function is_normal_file_buffer(bufnr)
-    local name = vim.api.nvim_buf_get_name(bufnr)
-
-    return vim.bo[bufnr].buftype == ""
-        and name ~= ""
-        and not name:match("^%w+://")
-        and not name:match("^CodeDiff ")
-        and not vim.bo[bufnr].filetype:match("^codediff")
-        and vim.fn.getftype(name) == "file"
-end
-
-local function should_persist_file_view(bufnr)
-    return is_normal_file_buffer(bufnr)
-        and not vim.w.codediff_restore
-        and not vim.wo.diff
-end
-
 autocmd("TextYankPost", {
     group = augroup("HighlightYank", { clear = true }),
     desc = "Highlight yanked text",
@@ -26,13 +9,43 @@ autocmd("TextYankPost", {
     end,
 })
 
+-- TODO: refactor into util file with buffers.lua
+local function is_normal_file_buffer(bufnr)
+    return vim.api.nvim_buf_is_valid(bufnr)
+        and vim.bo[bufnr].buftype == ""
+        and vim.api.nvim_buf_get_name(bufnr) ~= ""
+end
+
+local function window_has_codediff(winid)
+    return vim.w[winid].codediff_restore ~= nil
+end
+
+local function should_persist_file_view(bufnr, winid)
+    return is_normal_file_buffer(bufnr) and not window_has_codediff(winid)
+end
+
+local function open_window_folds(winid)
+    vim.api.nvim_win_call(winid, function() vim.wo.foldlevel = 99 end)
+end
+
+local function open_codediff_file_folds(bufnr, winid)
+    if not is_normal_file_buffer(bufnr) then
+        return
+    end
+
+    vim.defer_fn(function()
+        if window_has_codediff(winid) then
+            open_window_folds(winid)
+        end
+    end, 50)
+end
 local file_view_group = augroup("PersistFileView", { clear = true })
 
 autocmd("BufWinLeave", {
     group = file_view_group,
     desc = "Save file view state",
     callback = function(args)
-        if should_persist_file_view(args.buf) then
+        if should_persist_file_view(args.buf, vim.api.nvim_get_current_win()) then
             pcall(vim.cmd, "silent! mkview!")
         end
     end,
@@ -42,8 +55,12 @@ autocmd("BufWinEnter", {
     group = file_view_group,
     desc = "Restore file view state",
     callback = function(args)
-        if should_persist_file_view(args.buf) then
+        local winid = vim.api.nvim_get_current_win()
+
+        if should_persist_file_view(args.buf, winid) then
             pcall(vim.cmd, "silent! loadview")
+        else
+            open_codediff_file_folds(args.buf, winid)
         end
     end,
 })
