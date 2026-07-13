87bc883c9bb5b84316ead5a210bdeaf349250831
Phillipe Sebastiao <phillipe.sebastiao@gmail.com>
2026-07-13 17:40:17 +1000

    remove lint on leaving insert mode

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -2,7 +2,7 @@ return {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
-        vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
+        vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost" }, {
             group = vim.api.nvim_create_augroup("Lint", { clear = true }),
             callback = require("config.utils").on_normal_file_buffer(function(event)
                 vim.api.nvim_buf_call(event.buf, function()
